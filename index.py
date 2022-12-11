534ecc34573b5bec9f5aeb3dfa1a758ed5e4b14b
Eniola Hannah <heniola395@gmail.com>
2022-12-11 21:43:36 +0100

    I created the function 'mainMenu' whereby the user can choose the type of transaction he/she want to perform, and a conditional statement that's going to guide whatever the user type in

--- index.py
+++ index.py
@@ -9,3 +9,28 @@ class Atm:
         self.myBalance = 0.0
         self.customer_info = {}
         self.mainMenu()
+        
+    def mainMenu(self):
+        print("- WELCOME TO  " + self.name + " " + self.branch)
+        print("""
+
+        - What would you like to do
+        1. Open an account
+        2. Perform Transaction
+        3. Exit
+        """)
+        
+        self.user = input(">>  ")
+        if self.user == "1":
+            self.openAccount()
+        elif self.user == "2":
+            if self.customer_info == {}:
+                print("SORRY YOU DO NOT HAVE AN ACCOUNT WITH US \n CREATE AN ACCOUNT AND TRY AGAIN")
+                self.mainMenu()
+            else:
+                self.performTransaction()
+        elif self.user == "3":
+            self.exit()
+        else:
+            print("Wrong Input, please try again")
+            self.mainMenu()
